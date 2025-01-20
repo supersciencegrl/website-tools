441e7f8885cbfd955b402260574eb9590efa61f0
Carson, Nessa <nessa.carson@astrazeneca.com>
2025-01-20 20:55:22 +0000

    Update script documentation

--- search_content.py
+++ search_content.py
@@ -1,8 +1,14 @@
 ''' This script was created for the supersciencegrl.github.io repo. A more recent version may exist in supersciencegrl/supersciencegrl.github.io '''
 
+__author__ = "Nessa Carson"
+__copyright__ = "Copyright 2023"
+__version__ = "1.1"
+__email__ = "methionine57@gmail.com"
+__status__ = "Production"
+
 import glob
 
-def searchForQuery(pages, query, print_page_counts=False):
+def search_for_query(pages: list[str], query: str, print_page_counts=False):
     '''
     Searches through a list of html pages for the presence of a specified query string.
     Args:
@@ -12,22 +18,22 @@ def searchForQuery(pages, query, print_page_counts=False):
     Prints the file paths and number of instances of the query string found in each page.
     Also prints the total number of pages where the query string was found.
     '''
-    pagesFound = 0    
+    pages_found = 0    
     for page in pages:
         with open(page, 'r+', encoding='utf8') as fin:
             html = fin.read()
-            pageCount = html.count(query)
-            if pageCount:
+            page_count = html.count(query)
+            if page_count:
                 if print_page_counts:
-                    print(f'{page}:\t{pageCount} instances found in html.')
-                pagesFound += 1
+                    print(f'{page}:\t{page_count} instances found in html.')
+                pages_found += 1
 
-    if pagesFound:
-        print(f'\nTotal pages: {pagesFound}\n')
+    if pages_found:
+        print(f'\nTotal pages: {pages_found}\n')
     else:
         print('\nQuery not found in folder!\n')
 
-pages = glob.glob('**/*.html', recursive=True)
+pages = glob.glob('../**/*.html', recursive=True)
 exclusions = ['googlec9a765a08c18ee51.html', 'pinterest-fc74e.html', 'sandbox.html']
 for exc in exclusions:
     idx = pages.index(exc)
@@ -37,6 +43,6 @@ while True:
     query = input('Search query: ')
     if '-pages' in query or '--p' in query:
         query = query.replace(' --p', '')
-        searchForQuery(pages, query, print_page_counts=True)
+        search_for_query(pages, query, print_page_counts=True)
     else:
-        searchForQuery(pages, query, print_page_counts=False)
+        search_for_query(pages, query, print_page_counts=False)
--- update_batch.py
+++ update_batch.py
@@ -1,26 +1,31 @@
 ''' This script was created for the supersciencegrl.github.io repo. A more recent version may exist in supersciencegrl/supersciencegrl.github.io '''
 
+__author__ = "Nessa Carson"
+__copyright__ = "Copyright 2022"
+__version__ = "1.1"
+__email__ = "methionine57@gmail.com"
+__status__ = "Production"
+
 import glob
 
-def checkSublist(mylist, snippet):
+def find_index_in_sublist(snippet: list[str], my_list: list[str]) -> list[int]:
     """
-    Returns a list of indices where the `snippet` is found in the `mylist`.
+    Returns a list of indices where the `snippet` is found in `my_list`.
 
     Args:
-    - mylist (list): The list to search in.
-    - snippet (str): The substring to search for.
+    - snippet (list[str]): The substring to search for.
+    - mylist (list[str]): The list to search in.
 
     Returns:
-    - idxList (list): The list of indices where the `snippet` is found in the `mylist`.
+    - indices (list[int]): The list of indices where the `snippet` is found in `my_list`.
     """
-    
-    listLength = len(mylist)
-    snippetLength = len(snippet)
-    idxList = [i for i in range(listLength-snippetLength+1) if snippet == mylist[i:i+snippetLength]]
+    snippet_length = len(snippet)
+    list_length = len(my_list)
+    indices = [i for i in range(list_length-snippet_length+1) if snippet == my_list[i:i+snippet_length]]
 
-    return idxList
+    return indices
 
-def inputToHtml(my_input):
+def format_as_html(my_input: str) -> list[str]:
     """
     Formats the input into an list of HTML lines, with newlines at the end of each line.
 
@@ -28,55 +33,55 @@ def inputToHtml(my_input):
     - my_input (str): The input string.
 
     Returns:
-    - edited_list (list): The formatted HTML list.
+    - edited_list (list[str]): The formatted HTML list.
     """
-    
     mylist = my_input.split('\\n')
     edited_list = [f'{i}\n' for i in mylist]
 
     return edited_list
 
-def obtain_html_from_user():
+def obtain_html_from_user() -> tuple[list[str], list[str]]:
     """
     This function prompts the user to input old and new HTML data. It then converts the input to HTML format.
     
     Returns:
-    str: The old HTML data entered by the user.
-    str: The new HTML data entered by the user.
+    tuple[list[str], list[str]]: A tuple containing: 
+        str: The old HTML data entered by the user.
+        str: The new HTML data entered by the user.
     """
     print('WARNING: Recommend git push before editing for easy reversion to original.\n')
     
     oldhtml = ''
     while not oldhtml:
         oldhtml = input('Old html (copy tabs as tabs without changing; replace newlines with \'\\n\'): ')
-    oldhtml = inputToHtml(oldhtml)
+    oldhtml = format_as_html(oldhtml)
 
     newhtml = ''
     while not newhtml:
         newhtml = input('New html (copy tabs as tabs without changing; replace newlines with \'\\n\'): ')
-    newhtml = inputToHtml(newhtml)
+    newhtml = format_as_html(newhtml)
 
     return oldhtml, newhtml
 
-def run(pages, oldhtml, newhtml):
+def run(pages: list[str], oldhtml: list[str], newhtml: list[str]):
     """
     Replaces all instances of `oldhtml` with `newhtml` in the `pages` list of files.
     Prints a report of the changes made and the files that were not changed.
-    Note: BeautifulSoup etc does not maintain indentation by default
 
     Args:
-    - pages (list): A list of file paths to update.
-    - oldhtml (str): The old HTML code to replace.
-    - newhtml (str): The new HTML code to replace with.
+    - pages (list[str]): A list of file paths to update.
+    - oldhtml (list[str]): The old HTML code to replace.
+    - newhtml (list[str]): The new HTML code to replace with.
+
+    Note: BeautifulSoup is not used here, as it does not maintain indentation by default
     """
-    
     pages_changed = 0
     pages_not_changed = []
     for page in pages:
         with open(page, 'r+', encoding='utf8') as fin:
             html = fin.readlines()
 
-            idx_list = checkSublist(html, oldhtml)
+            idx_list = find_index_in_sublist(oldhtml, html)
             old_length = len(oldhtml)
             for idx in idx_list[::-1]:
                 html[idx:idx+old_length] = newhtml
@@ -95,7 +100,7 @@ def run(pages, oldhtml, newhtml):
     print(f'Pages not changed ({len(pages_not_changed)}):\n', pages_not_changed_string)
 
 # Set list of desired pages to change
-pages = glob.glob('**/*.html', recursive=True)
+pages = glob.glob('../**/*.html', recursive=True)
 exclusions = ['googlec9a765a08c18ee51.html', 'pinterest-fc74e.html']
 for exc in exclusions:
     idx = pages.index(exc)
